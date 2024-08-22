58364211c0a42aa2f7cf492fac4bf0e88f034c68
unknown <alexso2105@gmail.com>
2024-08-22 12:32:17 -0500

    new files

---
+++ print_math.py
@@ -0,0 +1,11 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Alex So
+# Section:      102
+# Assignment:   Lab 1.12 Print math
+# Date:         22/8/2024
+#
+
+print('Reynolds number is 5250.0\nWavelength is 0.03212028043565858 nm\nProduction rate is 2.8969356500320727 barrels/day\nChange of velocity is 571.1654292155197 m/s')
