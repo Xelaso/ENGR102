a05c81be9a501a9471b304c2ff900bc4965de1e3
unknown <alexso2105@gmail.com>
2024-08-22 22:16:02 -0500

    python_code_soup: create

---
+++ python_code_soup.py
@@ -0,0 +1,89 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Alexander So
+# Section:      573
+# Assignment:   Lab 2 Python Code Soup
+# Date:         22/8/2024
+#
+
+#usable functions
+x = 1
+y = 10
+z = 0
+
+
+z += x
+print(z)
+
+z += y
+z += y
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+print(z)
+z = 0
+
+x = y
+y *= x
+x = 1
+z += y
+z += x
+z += x
+print(z)
+
+y = 10
+x = y
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+z = 0
+z += y
+print(z)
+x = 1
+y = 10
+z = 0
+
+x = y
+y += x
+y += x
+y += x
+y += x
+y += x
+y += x
+y += x
+y += x
+x = 1
+y += x
+y += x
+y += x
+x = y
+y *= x
+x = y
+y = 10
+y += x
+x = y 
+y = 10
+y += x
+x = y
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+z += x
+print(z)
