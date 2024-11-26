2b7a1d5c9ad621a86e57fe64a4d116b3981a2088
unknown <alexso2105@gmail.com>
2024-11-25 23:10:03 -0600

    "Final Project/game.py":

--- Final Project/game.py
+++ Final Project/game.py
@@ -45,6 +45,211 @@ class RPS:
             print(result)
             print('-----------------------------------------------------------')
 
+class TTT:
+
+    title = "Tic Tac Toe"
+
+    #create blank board
+    def make_board(self, board):
+        for i in range(3):
+            print(" | ".join(board[i]))
+            if i < 2:                   #don't print dashes for the last line
+                print("-" * 9)
+
+
+    #declares winner once line is made
+    def winner(self, board, player):
+        for row in board:
+            if row[0] == row[1] == row[2] == player:
+                return True
+        for col in range(3):
+            if board[0][col] == board[1][col] == board[2][col] == player:
+                return True
+        if board[0][0] == board[1][1] == board[2][2] == player:
+            return True
+        if board[0][2] == board[1][1] == board[2][0] == player:
+            return True
+        return False
+
+
+    #create tic-tac-toe
+    def play_game(self):
+        myBoard = [[" "]*3 for i in range(3)]
+        player = "X"
+        counter = 0
+        while True:
+            counter += 1
+            self.make_board(myBoard)
+            placement = input(f"Player {player}, enter row and column seperated by a space (first row and column are index 0): ")
+            flag = False
+            while not flag:
+                try:
+                    row, col = int(placement.split()[0]), int(placement.split()[1])
+                    flag = True
+                except:
+                    print("Invalid input. Please try again")
+                    placement = input(f"Player {player}, enter row and column seperated by a space (first row and column are index 0): ")
+
+            
+            
+            if myBoard[row][col] == " ":
+                myBoard[row][col] = player
+                if self.winner(myBoard, player):
+                    self.make_board(myBoard)
+                    print(f"Player {player} wins! Congratulations!")
+                    break
+                tie = True
+                for row in myBoard:
+                    for cell in row:
+                        if cell == " ":
+                            tie = False
+                if tie:
+                    self.make_board(myBoard)
+                    print("It's a tie!")
+                    break
+                player = "O" if player == "X" else "X" #switch players
+            else:
+                print("Cell occupied. Try again.") 
+                
+    #call function to play game
+
+class Trivia:
+    title = "TAMU Trivia"
+    def play_game(self):
+        # Trivia game
+        score = 0
+
+        #print(' a: \n b: \n c: \n d: \n')
+
+        # Question 1
+        print('What year was Lawerence Sullivan Ross Born?')
+        print(' a: 1833\n b: 1838\n c: 1890\n d: 1852')
+        ans1 = input('Enter letter answer here: ')
+        if ans1 == 'b':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+            
+        # Question 2
+        print('When was the college of A&M established?')
+        print(' a: 1852\n b: 1876\n c: 1863\n d: 1862')
+        ans2 = input('Enter letter answer here: ')
+        if ans2 == 'd':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+
+
+        # Question 3
+        print('What day of the week was A&M opened for instruction?')
+        print(' a: Tuesday\n b: Thursday\n c: Wednesday\n d: Friday\n')
+        ans3 = input('Enter letter answer here: ')
+        if ans3 == 'c':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+            
+
+        # Question 4
+        print('What is NOT a Core Value of Texas A&M')
+        print(' a: Excellence\n b: Courage\n c: Leadership\n d: Respect\n')
+        ans4 = input('Enter letter answer here: ')
+        if ans4 == 'b':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+            
+        # Question 5
+        print('At what time did the bonfire stack collapse?')
+        print(' a: 2:42\n b: 4:28\n c: 3:57\n d: 1:30\n')
+        ans5 = input('Enter letter answer here: ')
+        if ans5 == 'a':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+            
+        # Question 6
+        print('How many Aggies served in World War II?')
+        print(' a: 14,000\n b: 20,000\n c: 16,405\n d: Over 20,000\n')
+        ans6 = input('Enter letter answer here: ')
+        if ans6 == 'd':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+            
+        # Question 7
+        print('When did Reville originally join the ranks of Texas A&M?')
+        print(' a: 1931\n b: 1940\n c: 1975\n d: 1890\n')
+        ans7 = input('Enter letter answer here: ')
+        if ans7 == 'a':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+            
+        # Question 8
+        print('What is the date of Aggie Muster?')
+        print(' a: April 27th\n b: April 19th\n c: March 21st\n d: April 21st\n')
+        ans8 = input('Enter letter answer here: ')
+        if ans8 == 'd':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+            
+        # Question 9
+        print('When was the first game between Texas A&M and tu played?')
+        print(' a: 1931\n b: 1899\n c: 1894\n d: 1902\n')
+        ans9 = input('Enter letter answer here: ')
+        if ans9 == 'c':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+
+        # Question 10
+        print('What class year was James Earl Rudder?')
+        print(' a: 1949\n b: 1971\n c: 1911\n d: 1932\n')
+        ans10 = input('Enter letter answer here: ')
+        if ans10 == 'd':
+            print('Correct')
+            print()
+            score += 1
+        else:
+            print('Incorrect')
+            print()
+
+
+        printscore = (f'| SCORE: {score * 10}%|')
+        print('-' * len(printscore))
+        print(printscore)
+        print('-' * len(printscore))
+        
+
 class Host:
     games = []
     def __init__(self, *args):
@@ -109,8 +314,8 @@ class Host:
                     print('\n-----------------------------------------------------------\nCredits')
                     print('    > Alexander So - Main Menu and Game Connection Logic; Host Class')
                     print('    > Clint Smith - Rock Paper Scissors Game; RPS Class')
-                    print('    > Garret Sumpter - ')
-                    print('    > Zane Aschenbeck - ')
+                    print('    > Garret Sumpter - Trivia Game; Trivia Class')
+                    print('    > Zane Aschenbeck - Tic Tac Toe Game; TTT Class')
                     print('-----------------------------------------------------------')
                     self.printMain()
                     action = self.processInput()
@@ -141,5 +346,6 @@ class Host:
 
 
 gameService = Host(RPS())
-
+gameService.addGame(TTT())
+gameService.addGame(Trivia())
 gameService.start()
